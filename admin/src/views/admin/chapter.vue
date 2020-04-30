8e558668b788eb9cc739e669ad0f456b8438dd1a
lishuyu01 <lishuyu01@qiyi.com>
2020-04-30 20:10:27 +0800

    1. 制作toast组件，内部用sweetalert2实现

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -131,6 +131,7 @@
           if (resp.success) {
             $("#form-modal").modal("hide");
             _this.list(1);
+            toast.success("保存成功！");
           }
         })
       },
@@ -152,11 +153,7 @@
               let resp = response.data;
               if (resp.success) {
                 _this.list(1);
-                Swal.fire(
-                  '删除成功!',
-                  '删除成功！',
-                  'success'
-                )
+                toast.success("删除成功！");
               }
             })
           }
