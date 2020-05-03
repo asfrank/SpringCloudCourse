e52389d07826bd26de7e5d90ee18bad6c44f43e1
lishuyu01 <lishuyu01@qiyi.com>
2020-05-03 14:02:08 +0800

    1. 小节表section表结构设计 2. 生成section表持久层代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -8,20 +8,22 @@ create table `chapter` (
 
 insert into `chapter` (id, course_id, name) values ('00000002', '00000002', '测试大章02');
 insert into `chapter` (id, course_id, name) values ('00000003', '00000003', '测试大章03');
-insert into `chapter` (id, course_id, name) values ('00000004', '00000004', '测试大章04');
-insert into `chapter` (id, course_id, name) values ('00000005', '00000005', '测试大章05');
-insert into `chapter` (id, course_id, name) values ('00000006', '00000006', '测试大章06');
-insert into `chapter` (id, course_id, name) values ('00000007', '00000007', '测试大章07');
-insert into `chapter` (id, course_id, name) values ('00000008', '00000008', '测试大章08');
-insert into `chapter` (id, course_id, name) values ('00000009', '00000009', '测试大章09');
-insert into `chapter` (id, course_id, name) values ('00000010', '00000010', '测试大章10');
-insert into `chapter` (id, course_id, name) values ('00000011', '00000011', '测试大章11');
-insert into `chapter` (id, course_id, name) values ('00000012', '00000012', '测试大章12');
-insert into `chapter` (id, course_id, name) values ('00000013', '00000013', '测试大章13');
-insert into `chapter` (id, course_id, name) values ('00000014', '00000014', '测试大章14');
-insert into `chapter` (id, course_id, name) values ('00000015', '00000015', '测试大章15');
-insert into `chapter` (id, course_id, name) values ('00000016', '00000016', '测试大章16');
-insert into `chapter` (id, course_id, name) values ('00000017', '00000017', '测试大章17');
-insert into `chapter` (id, course_id, name) values ('00000018', '00000018', '测试大章18');
-insert into `chapter` (id, course_id, name) values ('00000019', '00000019', '测试大章19');
-insert into `chapter` (id, course_id, name) values ('00000020', '00000020', '测试大章20');
+
+-- 小节
+DROP TABLE IF EXISTS `section`;
+CREATE TABLE `section` (
+                           `id` CHAR(8) NOT NULL DEFAULT '' COMMENT 'ID',
+                           `title` VARCHAR(50) NOT NULL COMMENT '标题',
+                           `course_id` CHAR(8) COMMENT '课程|course.id',
+                           `chapter_id` CHAR(8) COMMENT '大章|chapter.id',
+                           `video` VARCHAR(200) COMMENT '视频',
+                           `time` INT COMMENT '时长|单位秒',
+                           `charge` CHAR(1) COMMENT '收费|C 收费；F 免费',
+                           `sort` INT COMMENT '顺序',
+                           `created_at` DATETIME(3) COMMENT '创建时间',
+                           `updated_at` DATETIME(3) COMMENT '修改时间',
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='小节';
+
+INSERT INTO `section` (id, title, course_id, chapter_id, video, time, charge, sort, created_at, updated_at)
+VALUES ('00000001', '测试小节01', '00000001', '00000000', '', 500, 'F', 1, now(), now());
