5447f4a53cb6bc788605789f827850cff73fc7d3
lishuyu01 <lishuyu01@qiyi.com>
2020-05-05 20:52:19 +0800

    1. 大章管理与小节管理互相跳转 2. 小节管理保存和查询功能关联courseId，chapterId

--- admin/src/views/admin/section.vue
+++ admin/src/views/admin/section.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <h4 class="lighter">
+      <i class="ace-icon fa fa-hand-o-right icon-animated-hand-pointer blue"></i>
+      <router-link to="/business/course" class="pink"> {{course.name}} </router-link>：
+      <i class="ace-icon fa fa-hand-o-right icon-animated-hand-pointer blue"></i>
+      <router-link to="/business/chapter" class="pink"> {{chapter.name}} </router-link>
+    </h4>
+    <hr>
     <p>
       <button v-on:click="add()" class="btn btn-white btn-default btn-round">
         <i class="ace-icon fa fa-refresh"></i>
@@ -17,14 +24,12 @@
     <table id="simple-table" class="table  table-bordered table-hover">
       <thead>
       <tr>
-                    <th>ID</th>
-            <th>标题</th>
-            <th>课程</th>
-            <th>大章</th>
-            <th>视频</th>
-            <th>时长</th>
-            <th>收费</th>
-            <th>顺序</th>
+        <th>ID</th>
+        <th>标题</th>
+        <th>视频</th>
+        <th>时长</th>
+        <th>收费</th>
+        <th>顺序</th>
         <th>操作</th>
 
         <th></th>
@@ -35,8 +40,6 @@
       <tr v-for="section in sections">
           <td>{{section.id}}</td>
           <td>{{section.title}}</td>
-          <td>{{section.courseId}}</td>
-          <td>{{section.chapterId}}</td>
           <td>{{section.video}}</td>
           <td>{{section.time}}</td>
           <td>{{SECTION_CHARGE | optionKV(section.charge)}}</td>
@@ -73,13 +76,13 @@
                   <div class="form-group">
                       <label class="col-sm-2 control-label">课程</label>
                       <div class="col-sm-10">
-                          <input v-model="section.courseId" type="text" class="form-control">
+                        <p class="form-control-static">{{course.name}}</p>
                       </div>
                   </div>
                   <div class="form-group">
                       <label class="col-sm-2 control-label">大章</label>
                       <div class="col-sm-10">
-                          <input v-model="section.chapterId" type="text" class="form-control">
+                        <p class="form-control-static">{{chapter.name}}</p>
                       </div>
                   </div>
                   <div class="form-group">
@@ -132,12 +135,23 @@
         section: {},
         sections: [],
         SECTION_CHARGE: SECTION_CHARGE,
+        course: {},
+        chapter: {},
       }
     },
     mounted: function() {
       // this.$parent.activeSidebar("business-section-sidebar");
       let _this = this;
       _this.$refs.pagination.size = 5;
+
+      let course = SessionStorage.get("course") || {};
+      let chapter = SessionStorage.get("chapter") || {};
+      if (Tool.isEmpty(course) || Tool.isEmpty(chapter)) {
+        _this.$router.push("/welcome");
+      }
+      _this.course = course;
+      _this.chapter = chapter;
+
       _this.list(1);
     },
     methods: {
@@ -167,7 +181,9 @@
         Loading.show();
         _this.$ajax.post(process.env.VUE_APP_SERVER + "/business/admin/section/list", {
           page: page,
-          size: _this.$refs.pagination.size
+          size: _this.$refs.pagination.size,
+          courseId: _this.course.id,
+          chapterId: _this.chapter.id
         }).then((response)=>{
           Loading.hide();
           // console.log("查询小节列表结果：", response);
@@ -191,6 +207,9 @@
         ) {
           return;
         }
+
+        _this.section.courseId = _this.course.id;
+        _this.section.chapterId = _this.chapter.id;
 
         Loading.show();
         _this.$ajax.post(process.env.VUE_APP_SERVER + "/business/admin/section/save", _this.section).then((response)=>{
--- business/src/main/java/com/course/business/controller/admin/SectionController.java
+++ business/src/main/java/com/course/business/controller/admin/SectionController.java
@@ -1,19 +1,15 @@
 package com.course.business.controller.admin;
 
-import com.course.server.domain.Section;
-import com.course.server.dto.SectionDto;
-import com.course.server.dto.PageDto;
 import com.course.server.dto.ResponseDto;
-import com.course.server.exception.ValidatorException;
+import com.course.server.dto.SectionDto;
+import com.course.server.dto.SectionPageDto;
 import com.course.server.service.SectionService;
-import com.course.server.util.UuidUtil;
 import com.course.server.util.ValidatorUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/admin/section")
@@ -29,11 +25,12 @@ public class SectionController {
      * 列表查询
      */
     @PostMapping("/list")
-    public ResponseDto list(@RequestBody PageDto pageDto) {
-//        LOG.info("pageDto: {}", pageDto);
+    public ResponseDto list(@RequestBody SectionPageDto sectionPageDto) {
         ResponseDto responseDto = new ResponseDto();
-        sectionService.list(pageDto);
-        responseDto.setContent(pageDto);
+        ValidatorUtil.require(sectionPageDto.getCourseId(), "课程ID");
+        ValidatorUtil.require(sectionPageDto.getChapterId(), "大章ID");
+        sectionService.list(sectionPageDto);
+        responseDto.setContent(sectionPageDto);
         return responseDto;
     }
 
@@ -42,9 +39,7 @@ public class SectionController {
      */
     @PostMapping("/save")
     public ResponseDto save(@RequestBody SectionDto sectionDto) {
-//        LOG.info("sectionDto: {}", sectionDto);
-
-        //保存校验
+        // 保存校验
         ValidatorUtil.require(sectionDto.getTitle(), "标题");
         ValidatorUtil.length(sectionDto.getTitle(), "标题", 1, 50);
         ValidatorUtil.length(sectionDto.getVideo(), "视频", 1, 200);
@@ -60,7 +55,6 @@ public class SectionController {
      */
     @DeleteMapping("/delete/{id}")
     public ResponseDto delete(@PathVariable String id) {
-//        LOG.info("id: {}", id);
         ResponseDto responseDto = new ResponseDto();
         sectionService.delete(id);
         return responseDto;
